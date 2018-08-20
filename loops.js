d3356dfbd8162c02cf66a41bbc2a267ed060927f
Ava Barron <mztriz@gmail.com>
2018-08-20 18:43:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,8 +23,8 @@ function doWhileLoop(array){
   }
   do {
 		console.log('array.length = ' + array.length + ' and i = ' + i)
-		array = array.slice(1);
-		incrementVariable();
+		array = array.slice(1)
+		incrementVariable()
 	} while (array.length > 0 && i < 5)
 	return array
 }
